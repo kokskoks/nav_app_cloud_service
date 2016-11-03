f212248be442de641c37919f923c89b2c74cbdfb
Kadłubek <179640@edu.p.lodz.pl>
2016-11-03 21:09:08 +0100

    Created weeia parser

---
+++ src/main/java/pl/lodz/p/ind179640/service/parser/weeia/WeeiaParser.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.ind179640.service.parser.weeia;
+
+import org.springframework.stereotype.Component;
+
+import pl.lodz.p.ind179640.service.parser.Parser;
+
+@Component(value = "weeia")
+public class WeeiaParser implements Parser {
+
+	@Override
+	public void parse(byte[] planBytes) {
+		
+		
+	}
+
+}
